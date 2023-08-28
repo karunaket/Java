cfb5418c5db3d61a91458d46f8a44f17e6e2bebe
karuna ketan <karunaketan2077@gmail.com>
2023-08-28 18:48:35 +0545

    yupp

--- src/Chocolate.java
+++ src/Chocolate.java
@@ -1,5 +1,15 @@
 public class Chocolate {
     public static String iEatChocolates(){
+
         return "I Eat Chocolates";
+    }
+    public static void  doStuff(){
+        int a = 5;
+
+        while(a < 50)
+        {
+            System.out.println("Burger vs Pizza");
+            a++;
+        }
     }
 }
