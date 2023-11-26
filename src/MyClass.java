00a60fa8758470afed4b8394b4c85cde7c941eec
karuna ketan <karunaketan2077@gmail.com>
2023-11-27 01:12:02 +0530

    new

---
+++ src/MyClass.java
@@ -0,0 +1,8 @@
+// Define a package
+
+// Class within the package
+public class MyClass {
+    public static void main(String[] args) {
+        System.out.println("This is within a package.");
+    }
+}
